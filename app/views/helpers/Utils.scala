2e26cbe5e48f98851854cfad578f257e0a79e320
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2019-01-11 12:33:06 +0000

    CDSID-470 | enbolden file count

--- app/views/helpers/Utils.scala
+++ app/views/helpers/Utils.scala
@@ -30,6 +30,8 @@ object Utils {
     Html(values.toList.mkString("<br />"))
   }
 
+  def bold(value: Html): Html = bold(value.body)
+
   def bold(value: String): Html = {
     Html(s"<strong class='bold'>$value</strong>")
   }
@@ -37,6 +39,8 @@ object Utils {
   def email(address: String): Html = {
     Html(s"""<a href="email:$address">$address</a>""")
   }
+
+  def paragraph(content: Html): Html = paragraph(content.body)
 
   def paragraph(content: String): Html = {
     Html(s"<p>$content</p>")
