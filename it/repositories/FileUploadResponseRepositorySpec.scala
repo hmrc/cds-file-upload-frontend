e1679631f1e86bc351248fb12b182bcd7e8b5395
Gorkem Unsal <gorkem.unsal@accenture.com>
2019-03-18 09:57:16 +0000

    cache-fix file upload response repo unit tests

--- it/repositories/FileUploadResponseRepositorySpec.scala
+++ it/repositories/FileUploadResponseRepositorySpec.scala
@@ -1,13 +1,13 @@
 package repositories
 
 import models.requests.EORI
-import org.scalatest.{MustMatchers, OptionValues, WordSpec}
+import models.{File, FileUploadResponse, UploadRequest}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{MustMatchers, OptionValues, WordSpec}
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.json.{JsNull, JsString}
 import play.api.test.Helpers.running
 import suite.FailOnUnindexedQueries
-import uk.gov.hmrc.http.cache.client.CacheMap
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -15,7 +15,8 @@ class FileUploadResponseRepositorySpec extends WordSpec with MustMatchers
   with FailOnUnindexedQueries
   with ScalaFutures
   with IntegrationPatience
-  with OptionValues {
+  with OptionValues
+  with PropertyChecks {
 
   private lazy val builder: GuiceApplicationBuilder = new GuiceApplicationBuilder()
 
@@ -34,11 +35,65 @@ class FileUploadResponseRepositorySpec extends WordSpec with MustMatchers
           val fileUploadResponseRepo = app.injector.instanceOf[FileUploadResponseRepository]
 
           val test = for {
-            _      <- started(fileUploadResponseRepo)
+            _ <- started(fileUploadResponseRepo)
             result <- fileUploadResponseRepo.getAll(EORI("123"))
           } yield {
             result mustBe List.empty
           }
+          test.futureValue
+        }
+      }
+    }
+
+    "get the same values after a put" when {
+
+      "encryption is enabled" in {
+
+        database.map(_.drop()).futureValue
+
+        val app = builder.configure("mongodb.encryption-enabled" -> true).build()
+
+        running(app) {
+
+          val fileUploadResponseRepo = app.injector.instanceOf[FileUploadResponseRepository]
+
+          val testData = List(FileUploadResponse(List(File("reference", UploadRequest("href", Map("123" -> "123"))))))
+          val testEORI = EORI("123")
+
+          val test = for {
+            _      <- started(fileUploadResponseRepo)
+            _      <- fileUploadResponseRepo.put(testEORI, testData)
+            result <- fileUploadResponseRepo.getAll(testEORI)
+          } yield {
+            result mustBe testData
+          }
+
+          test.futureValue
+        }
+
+      }
+
+      "encryption is disabled" in {
+
+        database.map(_.drop()).futureValue
+
+        val app = builder.configure("mongodb.encryption-enabled" -> false).build()
+
+        running(app) {
+
+          val fileUploadResponseRepo = app.injector.instanceOf[FileUploadResponseRepository]
+
+          val testData = List(FileUploadResponse(List(File("reference", UploadRequest("href", Map("123" -> "123"))))))
+          val testEORI = EORI("123")
+
+          val test = for {
+            _      <- started(fileUploadResponseRepo)
+            _      <- fileUploadResponseRepo.put(testEORI, testData)
+            result <- fileUploadResponseRepo.getAll(testEORI)
+          } yield {
+            result mustBe testData
+          }
+
           test.futureValue
         }
       }
