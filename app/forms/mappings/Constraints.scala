51a27321269b48c5eca10865aaec8c4e15938eb6
Gorkem Unsal <gorkem.unsal@accenture.com>
2019-03-20 16:15:40 +0000

    CDSID-488 email fix

--- app/forms/mappings/Constraints.scala
+++ app/forms/mappings/Constraints.scala
@@ -22,7 +22,7 @@ trait Constraints {
 
   val isInt: String => Boolean = _.matches("^[0-9-]*$")
 
-  val isValidEmail:String => Boolean = _.matches("""^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""")
+  val isValidEmail:String => Boolean = _.matches("""^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-.]+$""")
   val isvalidPhoneNumber: String => Boolean = _.matches("""^[\d\s\+()\-/]+$""")
 
   protected def firstError[A](constraints: Constraint[A]*): Constraint[A] =
--- test/generators/Generators.scala
+++ test/generators/Generators.scala
@@ -26,7 +26,7 @@ trait Generators extends CacheMapGenerator with ModelGenerators with JsonGenerat
 
   implicit val dontShrink: Shrink[String] = Shrink.shrinkAny
 
-  val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"""
+  val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-.]+$"""
 
   val phoneRegex = """^[\d\s\+()\-/]+$"""
 
