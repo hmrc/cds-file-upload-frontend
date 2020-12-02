63ef7a16837aed41e911c30b0ea369888599aee3
pmonteiro <paulo.from.portugal@gmail.com>
2020-12-02 12:50:06 +0000

    CEDS-2798 Use relative URIs for stubed redirects

--- app/controllers/test/CustomsDeclarationsStubController.scala
+++ app/controllers/test/CustomsDeclarationsStubController.scala
@@ -41,7 +41,7 @@ class CustomsDeclarationsStubController @Inject()(appConfig: AppConfig, httpClie
 
     Waiting(
       UploadRequest(
-        href = s"$customsDeclarationsBaseUrl/cds-file-upload-service/test-only/s3-bucket",
+        href = "/cds-file-upload-service/test-only/s3-bucket",
         fields = Map(
           Algorithm.toString -> "AWS4-HMAC-SHA256",
           Signature.toString -> "xxxx",
@@ -49,8 +49,8 @@ class CustomsDeclarationsStubController @Inject()(appConfig: AppConfig, httpClie
           ACL.toString -> "private",
           Credentials.toString -> "ASIAxxxxxxxxx/20180202/eu-west-2/s3/aws4_request",
           Policy.toString -> "xxxxxxxx==",
-          SuccessRedirect.toString -> s"$customsDeclarationsBaseUrl/cds-file-upload-service/upload/upscan-success/${ref}",
-          ErrorRedirect.toString -> s"$customsDeclarationsBaseUrl/cds-file-upload-service/upload/upscan-error/${ref}"
+          SuccessRedirect.toString -> s"/cds-file-upload-service/upload/upscan-success/${ref}",
+          ErrorRedirect.toString -> s"/cds-file-upload-service/upload/upscan-error/${ref}"
         )
       )
     )
