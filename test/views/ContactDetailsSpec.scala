d8415fd01c2cf8f480f365eccf07df795d65f962
Tony Merryfield (Razer Blade 15) <tony.merryfield@oddbunch.co.uk>
2022-06-10 18:02:58 +0100

    CEDS-3586 - Added new attributes to phone number input

--- test/views/ContactDetailsSpec.scala
+++ test/views/ContactDetailsSpec.scala
@@ -82,7 +82,7 @@ class ContactDetailsSpec extends DomAssertions with ViewBehaviours with ScalaChe
 
       forAll { contactDetails: ContactDetails =>
         val popForm = form.fillAndValidate(contactDetails)
-        val input = exportsInputText(field = popForm("phoneNumber"), labelKey = "contactDetails.phoneNumber", labelClasses = "govuk-label")
+        val input = exportsInputText(field = popForm("phoneNumber"), labelKey = "contactDetails.phoneNumber", labelClasses = "govuk-label", inputmode = "numeric", pattern = "[0-9]*")
 
         viewAsString(popForm) must include(input.toString())
       }
