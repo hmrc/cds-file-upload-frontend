7c61696026907e12423c0cdc0ba9571450de51ab
Tim <glidester@gmail.com>
2022-10-17 11:11:50 +0100

    CEDS-4175 Make file upload validation case insensitive for file extensions

--- app/assets/javascripts/cdsfileuploadfrontend.js
+++ app/assets/javascripts/cdsfileuploadfrontend.js
@@ -2,7 +2,7 @@
 
 $.validator.addMethod('file-extensions', function (value, element, param) {
     return this.optional(element) || param.split(",").some(function(ext) {
-        return element.files[0].name.endsWith(ext) })
+        return element.files[0].name.toLowerCase().endsWith(ext) })
 }, 'File must have an ext of {0}');
 
 $.validator.addMethod('filesize', function (value, element, param) {
