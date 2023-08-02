499c8d0fcd982d6e605cb8872822f0a3ce7dce61
M Dweik <89045034+Dweik97@users.noreply.github.com>
2023-08-02 18:45:22 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     "uk.gov.hmrc"               %% "play-frontend-hmrc"         % "7.16.0-play-28",
     "uk.gov.hmrc"               %% "play-partials"              % "8.4.0-play-28",
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
-    "com.github.pureconfig"     %% "pureconfig"                 % "0.17.2"
+    "com.github.pureconfig"     %% "pureconfig"                 % "0.17.4"
   ).map(_.withSources)
 
   val test = Seq(
