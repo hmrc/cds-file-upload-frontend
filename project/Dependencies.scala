e6db6804a2c1ff9d40539b17c78b24969bbce92f
Lucio Biondi <lucio.biondi@digital.hmrc.gov.uk>
2024-02-19 16:23:08 +0000

    CEDS-5535 Use latest play-frontend-hmrc version, 8.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,11 @@ import sbt._
 object Dependencies {
 
   val bootstrapPlayVersion = "8.3.0"
+  val frontendPlayVersion = "8.5.0"
 
   val compile = Seq(
     "uk.gov.hmrc"               %% "bootstrap-frontend-play-30"     % bootstrapPlayVersion,
-    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % bootstrapPlayVersion,
+    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % frontendPlayVersion,
     "uk.gov.hmrc"               %% "play-partials-play-30"          % "9.1.0",
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"             % "1.7.0",
     "com.github.pureconfig"     %% "pureconfig"                     % "0.17.4"
