5cec4e8e97e2c5a974472aaca874e5d8bade35d1
sunil-sethumadhavan <171044301+sunil-sethumadhavan@users.noreply.github.com>
2025-07-02 17:50:12 +0100

    DCWL-3488: Addressed PR bot comments.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"               %% "bootstrap-frontend-play-30"     % bootstrapPlayVersion,
     "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.6.0",
-    "uk.gov.hmrc"               %% "play-partials-play-30"          % "10.0.0",
+    "uk.gov.hmrc"               %% "play-partials-play-30"          % "10.1.0",
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"             % "2.6.0",
     "com.github.pureconfig"     %% "pureconfig"                     % "0.17.7"
   ).map(_.withSources)
