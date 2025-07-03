cd37a5de4ece32b5266d36eab1237e705ed0b073
sunil-sethumadhavan <171044301+sunil-sethumadhavan@users.noreply.github.com>
2025-07-03 14:27:32 +0100

    DCWL-3488: Enable GOV.UK rebrand. Update service name. (#262)
    * DCWL-3488: Enable GOV.UK rebrand. Update service name.
    
    * DCWL-3488: Addressed PR bot comments.
    
    * DCWL-3488: Corrected welsh service name. Updated readme.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object Dependencies {
 
-  val bootstrapPlayVersion = "9.11.0"
+  val bootstrapPlayVersion = "9.13.0"
 
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"               %% "bootstrap-frontend-play-30"     % bootstrapPlayVersion,
-    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.0.0",
-    "uk.gov.hmrc"               %% "play-partials-play-30"          % "10.0.0",
+    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.6.0",
+    "uk.gov.hmrc"               %% "play-partials-play-30"          % "10.1.0",
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"             % "2.6.0",
     "com.github.pureconfig"     %% "pureconfig"                     % "0.17.7"
   ).map(_.withSources)
