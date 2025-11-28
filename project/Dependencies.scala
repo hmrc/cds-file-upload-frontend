0a246c95cef13e183376a984e7389034be48fc03
Tom Dennis <207199298+tomndennis@users.noreply.github.com>
2025-11-28 13:25:36 +0000

    DCWL-2994 - Related links not grouped using nav (#267)
    * DCWL-2994 - Related links not grouped using nav
    
    * DCWL-2994 - Remove comments and formatting fix.
    
    * DCWL-2994 - Dependency upgrade.
    
    ---------
    
    Co-authored-by: tomndennis <tomndennis@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"               %% "bootstrap-frontend-play-30"     % bootstrapPlayVersion,
-    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.20.0",
+    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.21.0",
     "uk.gov.hmrc"               %% "play-partials-play-30"          % "10.2.0",
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"             % "2.10.0",
     "com.github.pureconfig"     %% "pureconfig"                     % "0.17.7"
