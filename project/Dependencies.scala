8c5e9d56ced986d2be4489779e013afc8954d58a
Tom Dennis <207199298+tomndennis@users.noreply.github.com>
2026-03-04 09:06:01 +0000

    DCWL-4304 - use improved file input component. (#273)
    * DCWL-4304 - use improved file input component.
    
    * DCWL-4304 - use improved file input component.
    
    * DCWL-4304 - use improved file input component.
    
    * DCWL-4304 - dependency upgrades
    
    * DCWL-4304 - dependency upgrades
    
    ---------
    
    Co-authored-by: tomndennis <tomndennis@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object Dependencies {
 
-  val bootstrapPlayVersion = "10.4.0"
+  val bootstrapPlayVersion = "10.6.0"
 
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"               %% "bootstrap-frontend-play-30"     % bootstrapPlayVersion,
-    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.23.0",
+    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.31.0",
     "uk.gov.hmrc"               %% "play-partials-play-30"          % "10.2.0",
-    "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"             % "2.11.0",
+    "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"             % "2.12.0",
     "com.github.pureconfig"     %% "pureconfig-generic-scala3"      % "0.17.9"
   ).map(_.withSources)
 
