10eb41d73b082d0bbba895799d2ab94aa8d6bc64
sunil-sethumadhavan <171044301+sunil-sethumadhavan@users.noreply.github.com>
2026-03-25 11:17:10 +0000

    DCWL-4544: Dependency upgrade

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object Dependencies {
 
-  val bootstrapPlayVersion = "10.6.0"
+  val bootstrapPlayVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"               %% "bootstrap-frontend-play-30"     % bootstrapPlayVersion,
-    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.31.0",
+    "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30"     % "12.32.0",
     "uk.gov.hmrc"               %% "play-partials-play-30"          % "10.2.0",
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"             % "2.12.0",
     "com.github.pureconfig"     %% "pureconfig-generic-scala3"      % "0.17.9"
